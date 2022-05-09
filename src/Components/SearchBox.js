4986377d0f2f6046e019c62fe7e0a28cc598071e
vipulkandal <vipulkandal@gmail.com>
2022-05-09 22:12:20 +0530

    Search box

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./SearchBox.css";
 
 export default function SearchBox() {
   return (
     <>
-         <span>Here will be search box</span>
+      <div className='row search-box-outer-box'>
+        <div className='center'>
+          <div>
+            <li><a href="#buy">Buy</a></li>
+            <li><a href="#sell">Sell</a></li>
+            <li><a href="#rent">Rent</a></li>
+            <li><a href="#pg">PG / Co-living</a></li>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
